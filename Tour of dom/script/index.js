fb58461214f812b4246b9b50ce5fc7a7883a185c
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-13 21:46:25 +0600

    innertext chnage

--- Tour of dom/script/index.js
+++ Tour of dom/script/index.js
@@ -3,5 +3,8 @@
 
 let liCollection = document.getElementsByTagName('li');
 for(let li of liCollection){
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
+let placeTitle = document.getElementById('place-title');
+let placeChange = placeTitle.innerText
+console.log(placeChange)
