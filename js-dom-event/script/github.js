7af396eb8e7fa6771276e257f5f5f03306bd58d1
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-17 14:41:58 +0600

    another solution

--- js-dom-event/script/github.js
+++ js-dom-event/script/github.js
@@ -1,17 +1,35 @@
-document.getElementById('deleteBtn').addEventListener('click', function(){
-    let secretInfo = document.getElementById('secret-info');
-    secretInfo.style.display = 'none'
-})
+// document.getElementById('deleteBtn').addEventListener('click', function(){
+//     let secretInfo = document.getElementById('secret-info');
+//     secretInfo.style.display = 'none'
+// })
 
-document.getElementById('input-field').addEventListener('keyup', function(event){
-    let inputField = event.target.value;
+// document.getElementById('input-field').addEventListener('keyup', function(event){
+//     let inputField = event.target.value;
     
+//     let deleteBtn = document.getElementById('deleteBtn');
+//     if(inputField === 'delete'){
+//         deleteBtn.removeAttribute('disabled');
+//     }
+//     else{
+//         deleteBtn.setAttribute('disabled', true);
+//     }
+    
+// })
+let inputField = document.getElementById('input-field')
+inputField.addEventListener('keyup', function(event){
+    let inputValue = event.target.value
     let deleteBtn = document.getElementById('deleteBtn');
-    if(inputField === 'delete'){
+    if(inputValue === 'delete'){
         deleteBtn.removeAttribute('disabled');
     }
     else{
         deleteBtn.setAttribute('disabled', true);
     }
-    
 })
+
+document.getElementById('deleteBtn').addEventListener('click',function(){
+    let secretInfo = document.getElementById('secret-info');
+    secretInfo.style.display = 'none'
+    inputField.value = '';
+})
+
