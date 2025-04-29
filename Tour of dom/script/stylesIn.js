a69f21d63ea278df1c6f517a2527c14fcd949f6e
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-29 23:28:02 +0600

    again

--- Tour of dom/script/stylesIn.js
+++ Tour of dom/script/stylesIn.js
@@ -1,3 +1,15 @@
+// let sections = document.querySelectorAll('section');
+// for(let section of sections){
+//     section.style.border = '2px solid red';
+//     section.style.marginBottom = '10px';
+//     section.style.padding = '10px';
+//     section.style.borderRadius = '10px';
+//     section.style.backgroundColor = 'lightGray';
+// }
+
+// let placeList = document.getElementById('place');
+// // placeList.classList.add('bg-blue');
+// placeList.classList.add('bg-blue');
 let sections = document.querySelectorAll('section');
 for(let section of sections){
     section.style.border = '2px solid red';
