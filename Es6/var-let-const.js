3a431ceb4ec7539c2ddfea554860e64e5da3c23f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-04-30 20:47:15 +0600

    var-let-const problem solving

---
+++ Es6/var-let-const.js
@@ -0,0 +1,16 @@
+let count = 0;
+count = count + 10;
+// console.log(count
+
+const numbers = [1,2,3,4,5,6];
+// numbers = [22,33,44,55];
+numbers[2] = 66;
+numbers.push(0,99,88,77)
+// console.log(numbers);
+
+let sum = 0;
+for(let i = 0; i<10; i++){
+    sum = sum + i;
+    
+}
+console.log(sum);
