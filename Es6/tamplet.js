fcef5d20bac3a3403ab121d29c6d5424ed9b0dde
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-01 21:25:04 +0600

    tamplet

---
+++ Es6/tamplet.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+let total = `The sum of ${a} and ${b} is ${a+b}`;
+// console.log(total);
+
+let numbers = [11,12,13,14,15,16];
+let student = {
+    name:'Sabbir',
+    age:25
+}
+
+let math = `The sum of ${numbers[2]} and ${student.age} is ${numbers[2]+ student.age}`;
+console.log(math);
