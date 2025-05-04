1ad21c849f0af984b3f4aea7cce53fa9353612f3
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-05 02:56:49 +0600

    again

--- dom-problem-solving/problem_1.js
+++ dom-problem-solving/problem_1.js
@@ -1,3 +1,8 @@
 let numbers = (x,y,z) => x*y*z;
 let multiply = numbers(2,3,4);
 console.log(multiply); 
+
+let line = `I am a web developer.
+I love to code.
+I love to eat biryani`;
+console.log(line);
