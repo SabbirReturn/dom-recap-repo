eac591281ac963e35274eb217f0c984e9e1d51c8
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-05 04:34:09 +0600

    problem_2

--- dom-problem-solving/problem_1.js
+++ dom-problem-solving/problem_1.js
@@ -1,8 +1,13 @@
 let numbers = (x,y,z) => x*y*z;
 let multiply = numbers(2,3,4);
-console.log(multiply); 
+// console.log(multiply); 
 
 let line = `I am a web developer.
 I love to code.
 I love to eat biryani`;
-console.log(line);
+// console.log(line);
+
+
+let arrow = (x,y=2) => x+y;
+let sum = arrow(10);
+console.log(sum);
--- dom-problem-solving/problem_2.js
+++ dom-problem-solving/problem_2.js
@@ -1,3 +1,66 @@
-let arrow = (x,y=2) => x+y;
-let sum = arrow(10);
-console.log(sum);
+// let friends = ['sabbir','misu'];
+let newArray = []
+
+
+// let eventNames = (friends)=>{
+//     let newArray = [];
+//     // for(let friend of friends){
+
+//     // }
+// }
+
+// for( let i = 0; i<friends[0];i++){
+//     console.log(i);
+// }
+
+// for(let friend of friends[0]){
+//     console.log(friend);
+// }
+
+// let eventName = (x,y) =>{
+
+// }
+
+// // let friends = eventName([])
+// function eventName(names){
+//     let eventNewArray = [];
+//     let oddNewArray = [];
+//     for(let name of names){
+//         console.log(name)
+//         // let eventName = name.length;
+//         // console.log(eventName)
+//         // let splits = name.split('');
+//         // let length = splits.length
+//         // console.log(length)
+//         if(name.length % 2===0 ){
+//             eventNewArray.push(name);
+//         }
+//         else{
+//             oddNewArray.push(name)
+//         }
+//     }
+//     return{
+//         eventNewArray,
+//         oddNewArray
+//     }
+// }
+// let friends = eventName(['sabbir','hasan','misu'])
+// console.log(friends)
+
+
+let eventName = (names) =>{
+    let eventNewArray=[];
+    let oddNewArray=[];
+    for(let name of names){
+        if(name.length % 2===0){
+            eventNewArray.push(name)
+        }
+        else{
+            oddNewArray.push(name)
+        }
+    }
+    return{eventNewArray, oddNewArray};
+}
+
+let friend = eventName(['sabbir','hasan','misu'])
+console.log(friend);
