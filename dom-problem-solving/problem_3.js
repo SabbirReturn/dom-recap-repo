dc536c47cf153dc1403e6cb5478bee4d9aa74bc9
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-06 04:43:57 +0600

    problem_3

---
+++ dom-problem-solving/problem_3.js
@@ -0,0 +1,18 @@
+// Write an arrow function where it will do the following: x2;
+// a)Square each array element
+// b)Calculate the sum of squared element
+// Return the average of the sum of the squared element,
+
+
+let numbers = [2,3,4,5];
+let result = (numbers)=>{
+    let sum=0;
+    for( let number of numbers){
+        let square = Math.pow(number,2);
+        sum = sum + square
+        
+    }
+    return sum/numbers.length
+}
+let total = result(numbers);
+console.log(`The average of the sum is ${total}`);
