467484e29ee8118a9e627be488dd81d58ea0b34c
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-06 23:19:18 +0600

    problem_3

--- dom-problem-solving/problem_3.js
+++ dom-problem-solving/problem_3.js
@@ -5,6 +5,18 @@
 
 
 
+// let result = ()=>{
+//     let numbers = [2,3,4,5];
+//     let sum=0;
+//     for( let number of numbers){
+//         let square = Math.pow(number,2);
+//         sum = sum + square
+        
+//     }
+//     return sum/numbers.length
+// }
+// let total = result();
+// console.log(`The average of the sum is ${total}`);
 let result = ()=>{
     let numbers = [2,3,4,5];
     let sum=0;
