c32a53237a70f5815866f9c49e8328e821239c4d
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-12 22:53:58 +0600

    again

--- more-es6/filter.js
+++ more-es6/filter.js
@@ -42,6 +42,17 @@
 // let mxAge = age.find(x=>x>30);
 // console.log(mxAge);
 // // console.log(evenAge);
+// let friends = ['tom','sabbir','hasan'];
+
+// let oddFriend= friends.filter(friend=>friend.length % 2==1);
+// // console.log(oddFriend)
+
+// let age = [26,30,32,35,23,24,29];
+
+// let evenAge = age.filter(x=>x%2===0);
+// let mxAge = age.find(x=>x>30);
+// console.log(mxAge);
+// // console.log(evenAge);
 let friends = ['tom','sabbir','hasan'];
 
 let oddFriend= friends.filter(friend=>friend.length % 2==1);
