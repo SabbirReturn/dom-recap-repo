384bc59d28ce2b2da216518d005773d0f4fe192a
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-19 23:40:29 +0600

    again

--- Es6/key.js
+++ Es6/key.js
@@ -14,6 +14,22 @@
 
 // glass.isClean = 'clean'
 // console.log(glass);
+// let glass = {
+//     name : 'Glass',
+//     price: 5000,
+//     color: 'Golden',
+// }
+// console.log(glass);
+// let key = Object.keys(glass);
+// console.log(key);
+// let value = Object.values(glass);
+// console.log(value);
+
+// delete glass.color;
+// console.log(glass)
+
+// glass.isClean = 'clean'
+// console.log(glass);
 let glass = {
     name : 'Glass',
     price: 5000,
