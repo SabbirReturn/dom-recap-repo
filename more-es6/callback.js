763fbdcc01300db0b87c6c3fb29c0c98e87bf865
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-21 02:16:36 +0600

    callback

--- more-es6/callback.js
+++ more-es6/callback.js
@@ -1,8 +1,34 @@
-function greeting(greetingHandler,name){
-    greetingHandler(name);
-}
+// function greeting(greetingHandler,name){
+//     greetingHandler(name);
+// }
 
-function greetingHandler(name){
-    console.log('Good Morning',name)
+// function greetingHandler(name){
+//     console.log('Good Morning',name)
+// }
+// greeting(greetingHandler,'Sabbir')
+
+// function greeting(greetingHandler,name){
+//     greetingHandler(name)
+// }
+// function greetingEvening(name){
+//     console.log('Good Evening:', name)
+// }
+// greeting(greetingEvening,'sabbir');
+// function greeting(greetingHandler,name){
+//     greetingHandler(name);
+// }
+
+// function greetingEvening(name){
+//     console.log('Good Evening:', name)
+// }
+// greeting(greetingEvening,'sabbir')
+
+function greeting(greetingHandler,name){
+   return greetingHandler(name)
 }
-greeting(greetingHandler,'Sabbir')
+function greetingEvening(name){
+    // console.log('Good Evening:', name)
+    return `Good Evening: ${name}`
+}
+let outPut = greeting(greetingEvening,'Sabbir');
+console.log(outPut)
