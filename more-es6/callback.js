ebd9ad58f2005f0149f3695dcc57f57769785774
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-22 23:15:55 +0600

    again

--- more-es6/callback.js
+++ more-es6/callback.js
@@ -32,6 +32,15 @@
 // }
 // let outPut = greeting(greetingEvening,'Sabbir');
 // console.log(outPut)
+// function greeting(greetingHandler,name){
+//    return greetingHandler(name)
+// }
+// function greetingEvening(name){
+//     // console.log('Good Evening:', name)
+//     return `Good Evening: ${name}`
+// }
+// let outPut = greeting(greetingEvening,'Sabbir');
+// console.log(outPut)
 function greeting(greetingHandler,name){
    return greetingHandler(name)
 }
